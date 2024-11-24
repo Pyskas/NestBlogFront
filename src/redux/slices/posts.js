ad31a0448d9df3db30c2416579b5caf56896f2ca
Ivan Podoprigorov <ivan.podoprigorov@mail.ru>
2024-11-24 14:04:06 +0500

    Создал верстку,подключил Router,Toolkit

---
+++ src/redux/slices/posts.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    posts: {
+        items: [],
+        status: 'loading',
+    },
+    tags: {
+        items: [],
+        status: 'loading',
+    },
+};
+
+const postSlice = createSlice({
+    
+})
