e24a21213cf72558bfd9d7ffb0996220039d8088
Ivan Podoprigorov <ivan.podoprigorov@mail.ru>
2024-11-29 19:11:04 +0500

    Форма создания статьи и простое редактирование

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -6,10 +7,16 @@ import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/auth';
+import { Navigate } from 'react-router-dom';
 
 export const AddPost = () => {
   const imageUrl = '';
+  const isAuth = useSelector(selectIsAuth)
   const [value, setValue] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState('');
 
   const handleChangeFile = () => {};
 
@@ -34,6 +41,10 @@ export const AddPost = () => {
     [],
   );
 
+  if(!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to="/" />;
+  }
+  
   return (
     <Paper style={{ padding: 30 }}>
       <Button variant="outlined" size="large">
@@ -54,9 +65,18 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField
+      value={tags}
+      onChange={(e) => setTitle(e.target.value)}
+      classes={{ root: styles.tags }} 
+      variant="standard"
+       placeholder="Тэги"
+        fullWidth
+         />
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
